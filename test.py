42ce33b462b1fe05e47ca1351cca24655da3e57d
haddongik <haddongik@gmail.com>
2025-03-31 02:06:12 +0900

    코드정리

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ import os
 import asyncio
 from app.services.langchain_service import LangChainService
 from app.utils.app_utils import load_json, make_analysis_data
+from typing import Annotated, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+from langchain_teddynote.graphs import visualize_graph
+from app.config.app_config import app_config
+
+class State(TypedDict):
+    # 메시지 정의(list type 이며 add_messages 함수를 사용하여 메시지를 추가)
+    messages: Annotated[list, add_messages]
 
 async def test_report():
 
@@ -56,6 +66,50 @@ async def test_chat():
         # API 호출 간격 조절
         await asyncio.sleep(1)
 
+def test_langgraph():
+
+        
+    def chatbot(state: State):
+        # 메시지 호출 및 반환
+        return {"messages": [llm.invoke(state["messages"])]}
+
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        temperature=0,
+        openai_api_key=app_config["openai"]["api_key"]
+    )
+
+    # 그래프 생성
+    graph_builder = StateGraph(State)
+
+    # 노드 이름, 함수 혹은 callable 객체를 인자로 받아 노드를 추가
+    graph_builder.add_node("chatbot", chatbot)
+
+    ###### STEP 4. 그래프 엣지(Edge) 추가 ######
+    # 시작 노드에서 챗봇 노드로의 엣지 추가
+    graph_builder.add_edge(START, "chatbot")
+
+    # 그래프에 엣지 추가
+    graph_builder.add_edge("chatbot", END)
+
+    ###### STEP 5. 그래프 컴파일(compile) ######
+    # 그래프 컴파일
+    graph = graph_builder.compile()
+
+    ###### STEP 6. 그래프 시각화 ######
+    # 그래프 시각화
+    visualize_graph(graph)
+
+    ###### STEP 7. 그래프 실행 ######
+    question = "서울의 유명한 맛집 TOP 10 추천해줘"
+
+    # 그래프 이벤트 스트리밍
+    for event in graph.stream({"messages": [("user", question)]}):
+        # 이벤트 값 출력
+        for value in event.values():
+            print(value["messages"][-1].content)
+
 if __name__ == "__main__":
-    asyncio.run(test_report())
+    #asyncio.run(test_report())
     #asyncio.run(test_chat())
+    test_langgraph()
